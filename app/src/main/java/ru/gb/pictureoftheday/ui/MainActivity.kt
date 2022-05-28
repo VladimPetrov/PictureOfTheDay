e331c0938c2b33535140e0d301888e1b5e7610f3
Vladimir Petrov <vl.pva@yandex.ru>
2022-05-28 14:28:03 +0300

    change implementation apply theme

--- app/src/main/java/ru/gb/pictureoftheday/ui/MainActivity.kt
+++ app/src/main/java/ru/gb/pictureoftheday/ui/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.gb.pictureoftheday.R
 import ru.gb.pictureoftheday.databinding.ActivityMainBinding
 import ru.gb.pictureoftheday.domain.SharedPrefConst
@@ -15,26 +18,64 @@ import ru.gb.pictureoftheday.ui.otherpicture.TabsFragment
 import ru.gb.pictureoftheday.ui.pictureoftheday.FragmentPictureOfTheDay
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
-    private lateinit var binding:ActivityMainBinding
+    private var numberCurrentTheme: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val numberCurrentTheme =
+        numberCurrentTheme =
             this.getSharedPreferences(SharedPrefConst.SHARED_PREFS_NAME, Context.MODE_PRIVATE)
                 .getInt(SharedPrefConst.THEME_KEY, -1)
         if (numberCurrentTheme != -1) {
             setTheme(numberCurrentTheme)
         }
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bottomSheetBehavior =
+            setBottomSheetBehavior(findViewById(R.id.bottom_sheet_setting_container))
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.fragment_container_view, FragmentPictureOfTheDay())
                 .commit()
         }
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        bottomSheetBehavior = setBottomSheetBehavior(findViewById(R.id.bottom_sheet_setting_container))
+
+        initFAB()
+        initBottomNavigationView()
+        initRadioButton()
+
+    }
+
+    private fun initRadioButton() {
+        val settingRButton: RadioGroup? = findViewById(R.id.setting_radio_group)
+        when (numberCurrentTheme) {
+            R.style.Theme_Primary -> {
+                findViewById<RadioButton>(R.id.change_theme_radio_button_1)?.isChecked = true
+            }
+            R.style.Theme_Primary2 -> {
+                findViewById<RadioButton>(R.id.change_theme_radio_button_2)?.isChecked = true
+            }
+        }
+        settingRButton?.setOnCheckedChangeListener { group, checkedId ->
+            val sharedPref = getSharedPreferences(
+                SharedPrefConst.SHARED_PREFS_NAME,
+                Context.MODE_PRIVATE
+            )
+            val editor = sharedPref?.edit()
+            if (findViewById<RadioButton>(R.id.change_theme_radio_button_1)?.isChecked == true)
+                editor?.putInt(SharedPrefConst.THEME_KEY, R.style.Theme_Primary)
+
+            if (findViewById<RadioButton>(R.id.change_theme_radio_button_2)?.isChecked == true)
+                editor?.putInt(SharedPrefConst.THEME_KEY, R.style.Theme_Primary2)
+
+            editor?.apply()
+            recreate()
+        }
+    }
+
+    private fun initFAB() {
         binding.activityFabMain.setOnClickListener {
             when (bottomSheetBehavior.state) {
                 BottomSheetBehavior.STATE_COLLAPSED -> {
@@ -45,8 +86,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun initBottomNavigationView() {
         binding.mainBottomNavView.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.item_1_picture_of_the_day -> FragmentPictureOfTheDay()
                 R.id.item_2_other_picture -> TabsFragment()
                 else -> null
@@ -59,9 +103,13 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+
+
     private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout): BottomSheetBehavior<ConstraintLayout> {
         val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
         return bottomSheetBehavior
     }
+
+
 }
